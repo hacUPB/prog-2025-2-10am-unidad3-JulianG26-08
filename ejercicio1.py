9b46f6fd3832c930c8453cdfa08837e1eb400a42
julian galvez <julian.galvez@upb.edu.co>
2025-08-21 11:30:20 -0500

    ejercicio_2

--- ejercicio1.py
+++ ejercicio1.py
@@ -11,3 +11,22 @@ altura = float(altura)
 IMC = peso/altura**2
 #mostrar IMC
 print("tu IMC = ", IMC)
+# tabla de comparacion 
+if IMC >= 18.5 and IMC <= 24.9:
+    print ("peso normal")
+else:
+    if IMC >= 25 and IMC <= 29.9:
+        print ("tiene sobre pes0")
+    else:
+        if IMC >= 30 and IMC <= 34.9:
+            print("tiene obesidad I")
+        else:
+            if IMC > 35 and IMC <= 39.9:
+                print("tiene obesidad II")
+            else:
+                print("tiene obesidad extrema")
+                
+
+
+
+
