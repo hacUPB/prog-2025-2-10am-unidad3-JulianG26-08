fabdfe7142b55e9b9c1f1a77af207d30f6837410
julian Galvez <julian.galvez@upb.edu.co>
2025-09-01 23:32:14 -0500

    realizacion-de-analicis

---
+++ reto.py
@@ -0,0 +1,18 @@
+# reto unidad tres 
+## analisis de los casos 
+
+-1 como primer caso tenemos la presurizacion de las llantas siempre al inicio y al final de cada despegue necesitamos 
+verificar como minimo dos veces la presurizacion de las llanas ue no esten ni muy altas ni muy bajas 
+le pedimos al usuario que ingrese la presion que tienen las llantas en el momento y  ese reporte si enta dentro de
+180 y 220 psi  no pasa nada pero si no tiene que marcr una alerta para advertir que necesitan modificar la precion
+
+-2 como segudo caso tenemos la presion dentro de cabina  que varia dependiendo la altitud,
+En tierra La cabina está igualada con la presión exterior.
+Durante el ascenso La presión en cabina empieza a disminuir, pero de forma más lenta que la exterior.
+En crucero entre 10.000 y 12.000 m de altura  La cabina suele mantenerse equivalente a estar entre 1.500 y 2.400 m
+de altitud aproximadamente 10.9 y 11.6 psi
+Durante el descenso  La presión en cabina aumenta suavemente hasta igualar la exterior en el aterrizaje.
+
+-3 como ultimo caso tenemos el medidor de gasolina que es el que nos muestra cuanto nivel tenemos, y si es necesario 
+repostar gasolina en tierra, tambien tenemos una alarma que si se rebaja de sierto nivel la gasolina nos da un reporte 
+de que tenemos baja gasolina.
